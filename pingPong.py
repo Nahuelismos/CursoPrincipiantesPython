a02a3a496e211b68627d1aa3a59d16e5adf5bad2
Nahuel Chaves <nahuel_chaves_98@hotmail.com>
2025-11-10 18:10:23 -0300

    feat: Cambio valor negro pingPong

--- pingPong.py
+++ pingPong.py
@@ -9,7 +9,7 @@ VENTANA_HORI = 800  # Ancho de la ventana
 VENTANA_VERT = 600  # Alto de la ventana
 FPS = 60  # Fotogramas por segundo
 BLANCO = (255, 255, 255)  # Color del fondo de la ventana (RGB)
-NEGRO = (0, 0, 0)  # Color del texto (RGB)
+NEGRO = (0, 0, 1)  # Color del texto (RGB)
 
 
 class PelotaPong:
